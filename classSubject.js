756b7204bf95f3f7fd1522a18d6adec2c9df1b6b
haducacarlo1220 <ralphhaduca@gmail.com>
2024-11-04 19:02:57 +0800

    initial commit

---
+++ classSubject.js
@@ -0,0 +1,9 @@
+
+let subjectTitle = "Data Structures and Algorithms"; 
+let classSchedule = "9:30 AM - 11:00 AM, Monday & Friday";
+let classroom = "CLB";
+let classInstructor = "Sir Vitruvius John Barayuga";
+let studentName = "Ralph Carlo H. Pano";
+
+let script = studentName + " " + "is currently enrolled in" + " " + subjectTitle + " " + "with a class schedule of" + " " + classSchedule + " " + "at room" + " " + classroom + "." + " " + "The instructor for the subject is" + " " + classInstructor;
+console.log(script);
